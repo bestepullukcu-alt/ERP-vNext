1d05351436994f0e23b0898710925ba24193c158
Ali Tufanoglu <ali.tufanoglu@example.com>
2026-03-02 11:26:56 +0300

    FE: Page header added, footer text updated to 2018 | made with by Diten

--- frontend/Diten.Web/obj/Debug/net8.0/Diten.Web.WebUI.AssemblyInfo.cs
+++ frontend/Diten.Web/obj/Debug/net8.0/Diten.Web.WebUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Diten.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+552f858daa31209a80df07f013bdce170a0a6a83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e183445ec22681c8e533eee135e2e6971a1933fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Diten.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Diten.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
